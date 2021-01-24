89ef08be329ae072a797aea619708c3eb39cba69
karanamsridhar <karanamsridhar@gmail.com>
2021-01-23 19:32:59 -0600

    Added default tests

---
+++ src/test/java/com/sridhar/pipeline/DefaultTest.java
@@ -0,0 +1,11 @@
+package com.sridhar.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DefaultTest {
+    @Test
+    public void test(){
+    }
+}
